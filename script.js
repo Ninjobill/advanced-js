19d14e20528e4ddf2697b7766574b1d5dfebe48b
Billy Todd <billy.todd50@yahoo.com>
2019-08-10 15:36:46 -0700

    objects and functions

--- script.js
+++ script.js
@@ -61,6 +61,7 @@ var jane = Object.create(personProto,
 console.log(john);
 */
 
+/*
 // Primitive vs objects
 
 // primitives
@@ -101,6 +102,45 @@ change(age, obj);
 
 console.log(age);
 console.log(obj.city);
+*/
+
+// Lecture: passing functions as arguments
+
+var years = [1990, 1965, 1937, 2005, 1998];
+
+function arrayCalc(arr, fn) {
+    let arrRes = [];
+    for (let i = 0; i < arr.length; i++) {
+        arrRes.push(fn(arr[i]));
+    }
+    return arrRes;
+}
+
+function calculateAge(el) {
+    return 2016 - el;
+}
+
+function isFullAge (el) {
+    return el >= 18;
+}
+
+function maxHeartRate (el) {
+    if (el >= 18 && el <= 81) {
+        return Math.round(206.9 - (0.67 * el));
+    } else {
+        return -1;
+    }
+}
+
+var ages = arrayCalc(years, calculateAge);
+var fullAges = arrayCalc(ages, isFullAge);
+var maxHeartRate = arrayCalc(ages, maxHeartRate);
+
+
+console.log(ages);
+console.log(fullAges);
+console.log(maxHeartRate);
+
 
 
 
