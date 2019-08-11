d9612bec0045b5db4ba5f86624dcdf3d2175272e
Billy Todd <billy.todd50@yahoo.com>
2019-08-11 12:34:08 -0700

    closures

--- script.js
+++ script.js
@@ -186,7 +186,7 @@ howDeep('wide open')('diving')('diving deeper')('deeper')('bottom');
 */
 
 // IIFE
-
+/*
 function game() {
     var score = Math.random() * 10;
     console.log(score >= 5 );
@@ -198,14 +198,55 @@ game();
     var score = Math.random() * 10;
     console.log(score >= 5 );
 })();
+*/
+
+// **********************
+// Closures
+// ***********************
+
+function retirement(retirementAge) {
+    var a = ' years left until retirement.';
+    return function(yearOfBirth) {
+        var age = 2016 - yearOfBirth;
+        console.log((retirementAge - age) + a);
+    }
+}
+
+var retirementUS = retirement(66);
+var retirementGermany = retirement(65);
+var retirementIceland = retirement(67);
+
+retirementUS(1990);
+retirementGermany(1990);
+retirementIceland(1990);
+
+/*
+function interviewQuestion(job) { 
+    return function(name) {
+        console.log(name + ', hows your ' + job + ' job going?');
+    }
+    
+}
+
+var jane = interviewQuestion('Designer');
+var jim = interviewQuestion('Sales');
 
 
+jane('jane');
+jim('jim');
+*/
 
-
-
-
-
-
+function interviewQuestion(job) {
+    return function(name) {
+        if(job === 'designer') {
+            console.log(name + ', can you please explain what UX design is?');
+        } else if(job === 'teacher') {
+            console.log('What subject do you teach, ' + name + '?');
+        } else {
+            console.log('Hello ' + name + ', what do you do?');
+        }
+    }
+}
 
 // console.log(1 + 2 + "3");
 
